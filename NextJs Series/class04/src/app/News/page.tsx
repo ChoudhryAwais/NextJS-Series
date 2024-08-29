108852e44fde0d22c4b5d1d6ab0642d787c3e648
ChoudhryAwais <awaischoudhry306@gmail.com>
2024-08-29 22:51:01 +0500

    class 05

--- NextJs Series/class04/src/app/News/page.tsx
+++ NextJs Series/class04/src/app/News/page.tsx
@@ -1,9 +1,11 @@
+import Image from 'next/image'
 import React from 'react'
+import CarImage from "@/Image/car.jpg"
 
 export default function page() {
     return (
         <div>
-            News Page
+           <Image src={CarImage} alt='This is car image' width={100} height={100}/>
         </div>
     )
 }
---
+++ NextJs Series/class05/src/app/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <div className="flex justify-between">
+      <div className="size-20 bg-blue-700 rounded-full"></div>
+      <div className="size-20 bg-red-700 rounded-full"></div>
+      <div className="size-20 bg-purple-700 rounded-full"></div>
+      <div className="size-20 bg-purple-700 rounded-full"></div>
+
+    </div>
+  );
+}
